a63eaf6ca77c218b437c16092dcd944d96703dc2
YerayPadial <yeraykeira@gmail.com>
2026-03-25 13:10:06 +0100

    feat: Added new images and videos AND added the restriction level up 1.7

--- guia/src/screens/HomeScreen.jsx
+++ guia/src/screens/HomeScreen.jsx
@@ -5,7 +5,7 @@ import { Dumbbell } from "lucide-react";
 
 const HomeScreen = ({ onSelectLevel }) => {
   // Lista de IDs que quieres mostrar
-  const idsVisibles = [1, 2, 3, 5];
+  const idsVisibles = [1, 2, 3, 5]; // id 4 para la rutina avanzada
 
   return (
     <div className="bg-fondo-claro dark:bg-fondo-oscuro min-h-screen p-4 pt-8">
